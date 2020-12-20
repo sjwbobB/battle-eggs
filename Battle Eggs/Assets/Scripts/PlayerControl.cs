703076488eafb4c14aefcdb515980befc44f47f7
akabomb-prog <69766525+akabomb-prog@users.noreply.github.com>
2020-12-20 19:48:11 +0200

    Create player control script
    Create a player GameObject and assign this script to it.
    Must have a Rigidbody 2D.

---
+++ Battle Eggs/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    #region "Variables"
+    public Rigidbody2D rigid;
+    public float moveSpeed;
+    #endregion
+
+    void Start()
+    {
+        // Setup Rigidbody 2D
+        rigid.freezeRotation = true;
+    }
+
+    void Update()
+    {
+        // Move player left and right
+        float leftright = Input.GetAxisRaw("Horizontal");
+        rigid.MovePosition(new Vector2(leftright * moveSpeed * Time.deltaTime, 0.0f));
+    }
+}
