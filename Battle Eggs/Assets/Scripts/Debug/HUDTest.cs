66dfa63061cf52d7578913964a21ccc7a6462f38
akabomb-prog <69766525+akabomb-prog@users.noreply.github.com>
2020-12-22 23:51:39 +0200

    Several changes #4
    - Added HUDTest and Screenshot debug scripts (Screenshot possibly useful out of debug?)
    - Updated .gitignore

---
+++ Battle Eggs/Assets/Scripts/Debug/HUDTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDTest : MonoBehaviour
+{
+    #region "Variables"
+    public Texture2D texture;
+    public Vector2 size;
+    public bool relative;
+    #endregion
+
+    private Vector2 screenSize;
+
+    void Update()
+    {
+        screenSize = new Vector2(Screen.width, Screen.height);
+    }
+
+    void OnGUI()
+    {
+        // Draw specified texture, with size being relative based on the "Relative" checkbox
+        GUI.DrawTexture(new Rect(Vector2.zero, relative ? size * screenSize : size), texture);
+    }
+}
---
+++ Battle Eggs/Assets/Scripts/Debug/Screenshot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screenshot : MonoBehaviour
+{
+    #region "Variables"
+    public string fileName = "screenshot.png";
+    #endregion
+
+    void FixedUpdate()
+    {
+        // Capture screenshots with F12
+        if (Input.GetKeyDown(KeyCode.F12))
+            ScreenCapture.CaptureScreenshot("Assets/Screenshots/" + fileName);
+    }
+}
