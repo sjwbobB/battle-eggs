57629cabb86ae368707b8fdc01707420a41081f1
akabomb-prog <69766525+akabomb-prog@users.noreply.github.com>
2020-12-22 21:08:12 +0200

    Create script for object following
    Comment FPSCounter
    Assign ObjectFollow script to scene's camera

--- Battle Eggs/Assets/Scripts/Debug/FPSCounter.cs
+++ Battle Eggs/Assets/Scripts/Debug/FPSCounter.cs
@@ -7,21 +7,28 @@ public class FPSCounter : MonoBehaviour
 
 	void Update()
 	{
+		// Update delta time every frame
 		deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
 	}
 
 	void OnGUI()
 	{
+		// Get ms and fps
+		float msec = deltaTime * 1000.0f;
+		float fps = 1.0f / deltaTime;
+
 		int w = Screen.width, h = Screen.height;
 
+		// Set style for label font
 		GUIStyle style = new GUIStyle();
-
-		Rect rect = new Rect(0, 0, w, h * 2 / 100);
 		style.alignment = TextAnchor.UpperLeft;
 		style.fontSize = 24;
 		style.normal.textColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
-		float msec = deltaTime * 1000.0f;
-		float fps = 1.0f / deltaTime;
+
+		// Rectangle for label drawing
+		Rect rect = new Rect(0, 0, w, style.fontSize);
+
+		// Draw label with text "<ms> ms (<fps> fps)"
 		string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
 		GUI.Label(rect, text, style);
 	}
---
+++ Battle Eggs/Assets/Scripts/ObjectFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFollow : MonoBehaviour
+{
+    #region "Variables"
+    public GameObject objectToFollow;
+    public float lerpScale;
+    #endregion
+
+    private float initialZ;
+
+    void Start()
+    {
+        initialZ = transform.position.z;
+    }
+
+    void FixedUpdate()
+    {
+        // Create placeholder Vector3 for modification
+        Vector3 vec = transform.position;
+
+        // Interpolate vec to the other object's position (while keeping z at the initial value)
+        vec = Vector3.Lerp(vec, objectToFollow.transform.position, lerpScale * Time.deltaTime);
+        vec.z = initialZ;
+
+        // Set this object's position to vec
+        transform.position = vec;
+    }
+}
